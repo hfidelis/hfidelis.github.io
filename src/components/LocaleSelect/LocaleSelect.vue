1771b868aec8ef10ca6e96edfa303b209e842c97
hfidelis <heitor@coorlab.com.br>
2024-02-23 23:33:12 -0300

    feat: update dark/light mode to LocaleSelect

--- src/components/LocaleSelect/LocaleSelect.vue
+++ src/components/LocaleSelect/LocaleSelect.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { useDark } from '@vueuse/core'
+
 import VueMultiselect from 'vue-multiselect'
 
 import Locale from '@/types/i18n/Locale'
@@ -11,6 +13,8 @@ export default {
     LocaleOption,
   },
   data() {
+    const isDark = useDark()
+
     const selectedLocale: LocaleValueType = {
       value: this.$i18n.locale as Locale,
       flag: '🇧🇷',
@@ -29,6 +33,7 @@ export default {
     })
 
     return {
+      isDark,
       locales,
       countryFlags,
       selectedLocale,
@@ -45,40 +50,107 @@ export default {
 </script>
 
 <template>
-  <VueMultiselect
-    :options="locales"
-    :selectLabel="''"
-    :deselectLabel="''"
-    :allowEmpty="false"
-    :searchable="false"
-    :selectedLabel="$t('components.localeSelect.selectedLabel')"
-    :model-value="selectedLocale"
-    @update:model-value="updateLocale"
+  <div
+    class="select__wrapper"
+    :class="isDark ? 'dark__select' : 'light__select'"
   >
-
-    <template
-      v-slot:singleLabel="props"
+    <VueMultiselect
+      :options="locales"
+      :selectLabel="''"
+      :deselectLabel="''"
+      :allowEmpty="false"
+      :searchable="false"
+      :selectedLabel="$t('components.localeSelect.selectedLabel')"
+      :model-value="selectedLocale"
+      @update:model-value="updateLocale"
     >
-      <LocaleOption
-        :option="props.option"
-      />
-    </template>
-
-    <template
-      v-slot:option="props"
-    >
-      <LocaleOption
-        :option="props.option"
-      />
-    </template>
-
-  </VueMultiselect>
+      <template
+        v-slot:singleLabel="props"
+      >
+        <LocaleOption
+          :option="props.option"
+        />
+      </template>
+      <template
+        v-slot:option="props"
+      >
+        <LocaleOption
+          :option="props.option"
+        />
+      </template>
+    </VueMultiselect>
+  </div>
 </template>
 
-<style src="vue-multiselect/dist/vue-multiselect.css" />
+<style lang="scss" scoped>
+@import '@/styles/app.scss';
 
-<style lang="scss">
+.select__wrapper,
 .multiselect {
   width: fit-content;
 }
 </style>
+
+<style lang="scss">
+@import '@/styles/app.scss';
+
+.dark__select {
+  border: $border-sm solid $dark-border;
+  border-radius: $radius-md;
+  box-shadow: $dark-mode-shadow;
+
+  .multiselect {
+    background-color: transparent;
+    color: $primary-light;
+    border: none;
+
+    .multiselect__tags,
+    .multiselect__single,
+    .multiselect__content-wrapper {
+      background-color: $primary-dark;
+      color: $primary-light;
+      border: none;
+    }
+  }
+
+  .multiselect > .multiselect__content-wrapper {
+    border: $border-sm solid $dark-border;
+    box-shadow: $dark-mode-shadow;
+  }
+
+  .multiselect > .multiselect__select::before {
+    border-top-color: $dark-border;
+  }
+}
+
+.light__select {
+  border: $border-sm solid $light-border;
+  border-radius: $radius-md;
+  box-shadow: $light-mode-shadow;
+
+  .multiselect {
+    background-color: transparent;
+    color: $primary-dark;
+    border: none;
+
+    .multiselect__tags,
+    .multiselect__single,
+    .multiselect__content-wrapper {
+      background-color: $primary-light;
+      color: $primary-dark;
+      border: none;
+    }
+  }
+
+  .multiselect > .multiselect__content-wrapper {
+    border: $border-sm solid $light-border;
+    box-shadow: $light-mode-shadow;
+  }
+
+  .multiselect > .multiselect__select::before {
+    border-top-color: $light-border;
+  }
+}
+</style>
+
+<style src="vue-multiselect/dist/vue-multiselect.css" />
