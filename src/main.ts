f74f4a0cfa68a204e7718dcd617a953ee12b08ba
hfidelis <heitor@coorlab.com.br>
2024-02-24 01:31:02 -0300

    chore: register svgicon globally

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import '@/styles/app.scss'
 
+import SvgIcon from '@jamescoyle/vue-icon'
+
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
@@ -8,4 +10,5 @@ import i18n from '@/i18n'
 createApp(App)
   .use(router)
   .use(i18n)
+  .component('SvgIcon', SvgIcon)
   .mount('#app')
