2b1eba3c427c93f514e67bee861b0cb25e562508
hfidelis <heitor@coorlab.com.br>
2024-08-19 00:29:16 -0300

    refactor(App): use FadeTransition component

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,18 @@ import { RouterView } from 'vue-router'
 
 import AppHeader from '@/components/header/AppHeader.vue'
 import AppFooter from '@/components/footer/AppFooter.vue'
+import FadeTransition from '@/components/fade-transition/FadeTransition.vue'
 </script>
 
-<template>
-  <div>
-    <AppHeader />
-    <RouterView
-      v-slot="{
-        Component,
-        route
-      }"
-    >
-      <Transition
-        name="fade"
-        mode="out-in"
-      >
-        <component
-          :is="Component"
-          :key="route.path"
-        />
-      </Transition>
-    </RouterView>
-    <AppFooter />
-  </div>
+<template>  
+  <AppHeader />
+  <RouterView v-slot="{ Component, route }">
+    <FadeTransition>
+      <component
+        :is="Component"
+        :key="route.path"
+      />
+    </FadeTransition>
+  </RouterView>
+  <AppFooter />
 </template>
-
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.45s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
