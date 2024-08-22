bbbe9f62a57397abd61077992f09de3b5b007801
hfidelis <heitor@coorlab.com.br>
2024-08-22 00:47:25 -0300

    refactor(theme-toggle): componized styles

--- src/components/theme-toggle/ThemeToggle.vue
+++ src/components/theme-toggle/ThemeToggle.vue
@@ -19,13 +19,14 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <button
-    :aria-label="$t('components.themeToggle.ariaLabel')"
-    class="theme__toggle__btn"
+  <button    
+    id="theme__toggle__btn"
+    class="header__button"
     :class="[
-      isDark ? 'dark__toggle' : 'light__toggle',
+      isDark ? 'dark' : 'light',
       isMobile ? 'header__mobile__item' : '',
     ]"
+    :aria-label="$t('components.themeToggle.ariaLabel')"
     @click="toggleDark()"
   >
     <span
@@ -44,45 +45,17 @@ const toggleDark = useToggle(isDark)
 <style lang="scss" scoped >
 @import '@/styles/app.scss';
 
-.theme__toggle__btn {
-  @include flex(row, center, center, 0.6rem);
-  border: none;
-  border-radius: $radius-md;
-  padding: 0.2rem 0.6rem;
-  cursor: pointer;
-  overflow: hidden;
-  height: $header-item-height;
-  transition: all 0.45s ease;
-
-  > span {
-    font-size: $text-sm;
-    font-weight: 600;
+#theme__toggle__btn {
+  &.dark {
+    > svg {
+      color: $primary-light;
+    }
   }
 
-  &:hover {
-    transform: scale(1.05);
-    box-shadow: 0 0 0.35rem $darker-cyan,
-                0 0 0.6rem $cyan;
-  }
-}
-
-.dark__toggle {
-  background-color: $primary-dark;
-  border: $border-sm solid $dark-border;
-  box-shadow: $dark-mode-shadow;
-
-  > svg, span {
-    color: $primary-light;
-  }
-}
-
-.light__toggle {
-  background-color: $primary-light;
-  border: $border-sm solid $light-border;
-  box-shadow: $light-mode-shadow;
-
-  > svg, span {
-    color: $primary-dark;
+  &.light{
+    > svg {
+      color: $primary-dark;
+    }
   }
 }
 </style>
