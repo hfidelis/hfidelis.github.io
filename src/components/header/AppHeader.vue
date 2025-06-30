1ab43a82ce2b50ff7dc15729cae8c4b45f77a962
hfidelis <heitorc88@gmail.com>
2025-06-30 01:29:31 -0300

    style(header): use tw classes

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -28,11 +28,11 @@ watch(
 
 <template>
   <header
-    class="app__header"
-    :class="isDark ? 'dark__header' : 'light__header'"
+    class="flex flex-row items-center justify-between w-full h-[10dvh] px-7 lg:px-[15dvw] xl:px-[25dvw]"
+    :class="isDark ? 'border-b-2 border-secondary-dark shadow-md' : 'border-b-2 border-secondary-light shadow-md'"
   >
     <div
-      class="app__header__logo"
+      class="flex flex-row items-center justify-center md:text-[1.75rem] text-2xl font-bold"
     >
       <span
         class="bg-gradient-to-r text-gradient"
@@ -58,11 +58,12 @@ watch(
       </span>
     </div>
     <section
-      class="app__header__actions"
+      class="flex flex-row items-center justify-center"
     >
       <button
         v-if="isMobile"
-        class="menu__button"
+        class="flex flex-row items-center justify-center cursor-pointer rounded-md p-2"
+        :class="isDark ? 'text-white bg-primary-dark border border-secondary-dark shadow-md' : 'text-black bg-primary-light border border-secondary-light shadow-md'"
         :aria-label="
             isOpen ?
             $t('components.header.menuButton.actions.close') :
@@ -78,18 +79,24 @@ watch(
       </button>
       <div
         v-if="!isMobile"
-        class='actions__default'
+        class='flex flex-row items-center justify-center gap-3'
       >
         <BlogButton />
         <LocaleSelect />
         <ThemeToggle />
       </div>
       <Transition
-        name="sidebar"
+        enter-active-class="transition-transform duration-500 ease-in-out"
+        enter-from-class="transform translate-x-full"
+        enter-to-class="transform translate-x-0"
+        leave-active-class="transition-transform duration-500 ease-in-out"
+        leave-from-class="transform translate-x-0"
+        leave-to-class="transform translate-x-full"
       >
         <div
           v-if="isMobile && isOpen"
-          class='actions__mobile'
+          class='absolute top-[10dvh] left-0 z-50 flex flex-col items-end justify-start w-full min-h-screen gap-3 p-4 bg-gradient-to-br opacity-95'
+          :class="isDark ? 'from-primary-dark to-primary-dark via-gray-900' : 'from-primary-light to-primary-light via-neutral-400'"
         >
           <BlogButton />
           <LocaleSelect />
@@ -99,149 +106,3 @@ watch(
     </section>
   </header>
 </template>
-
-<style scoped lang="scss">
-@import '@/styles/app.scss';
-
-.app__header {
-  @include flex(row, center, space-between);
-  width: 100dvw;
-  height: $header-height;
-  padding: 1.2rem 25dvw;
-
-  @media screen {
-    @media (max-width: 1400px) {
-      padding: 1.2rem 15dvw;
-    }
-
-    @media (max-width: 1024px) {
-      padding: 1.2rem 10dvw;
-    }
-
-    @media (max-width: 768px) {
-      padding: 1.2rem 1.8rem;
-    }
-  }
-}
-
-.dark__header {
-  border-bottom: 2px solid $dark-border;
-  box-shadow: $dark-mode-shadow;
-
-  .app__header__actions {
-    > .menu__button {
-      color: $white;
-      background-color: $primary-dark;
-      border: $border-sm solid $dark-border;
-      box-shadow: $dark-mode-shadow;
-    }
-  }
-}
-
-.light__header {
-  border-bottom: 2px solid $light-border;
-  box-shadow: $light-mode-shadow;
-
-
-  .app__header__actions {
-   > .menu__button {
-      color: $black;
-      background-color: $primary-light;
-      border: $border-sm solid $light-border;
-      box-shadow: $light-mode-shadow;
-    }
-  }
-}
-
-.logo__prefix {
-  background-image: linear-gradient(90deg, $cyan, $dark-cyan);
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-}
-
-.app__header__logo {
-  @include flex(row, center, center);
-  font-size: $text-xl;
-  font-weight: 700;
-
-  > a {
-    color: $white;
-    text-decoration: none;
-  }
-
-  @media screen {
-    @media (max-width: 768px) {
-      font-size: $text-lg;
-    }
-  }
-}
-
-.logo__thick {
-  position: relative;
-  width: 0.8rem;
-  height: $text-xl;
-  margin-left: 0.4rem;
-
-  @media screen {
-    @media (max-width: 768px) {
-      height: $text-lg;
-    }
-  }
-}
-
-.app__header__actions, .actions__default {
-  @include flex(row, center, center, 0.8rem);
-}
-
-.menu__button {
-  @include flex(row, center, center);
-  cursor: pointer;
-  border-radius: $radius-md;
-  padding: 0.4rem 0.6rem;
-}
-
-.actions__mobile {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-end;
-  gap: 0.8rem;
-  padding: 1.2rem 2rem;
-
-  min-height: 100dvh;
-  width: 100dvw;
-
-  position: absolute;
-  top: $header-height;
-  left: 0;
-  z-index: 99;
-
-  background-color: rgb(0 0 0 / 85%);
-  animation: enter 0.5s ease;
-
-  backdrop-filter: blur(0.375rem) grayscale(0.75);
-  -webkit-backdrop-filter: blur(0.375rem) grayscale(0.75);
-}
-
-@keyframes enter {
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(0);
-  }
-}
-
-.sidebar-leave-active {
-  transition: transform 0.5s;
-}
-.sidebar-leave-to {
-  transform: translateX(100%);
-}
-
-
-.sidebar-leave-active {
-  transition: transform 0.5s, right 0.5s;
-}
-</style>
--- src/types/components/locale-select/LocaleValue.type.ts
+++
@@ -1,8 +0,0 @@
-import Locale from "@/types/i18n/Locale"
-
-type LocaleValue = {
-  value: Locale,
-  flag: string,
-}
-
-export default LocaleValue
