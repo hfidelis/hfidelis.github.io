9635d6984e86b8da2493aa61a7705702e9c02b53
hfidelis <heitorc88@gmail.com>
2025-07-03 02:59:05 -0300

    styles(home): use divider from naive ui

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import AuthorInfo from '@/components/author-info/AuthorInfo.vue'
 import TechTags from '@/components/tech-tags/TechTags.vue'
+
+import { NDivider } from 'naive-ui';
 </script>
 
 <template>
   <Layout>
-    <div class="flex flex-col items-center justify-center gap-[4rem]">
+    <div class="flex flex-col items-center justify-center">
       <AuthorInfo />
+      <NDivider />
       <TechTags />
     </div>
   </Layout>
