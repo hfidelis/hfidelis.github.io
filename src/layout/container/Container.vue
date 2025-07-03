21c54407e3f208594d970a7ada2b695b42569308
hfidelis <heitorc88@gmail.com>
2025-07-03 02:58:07 -0300

    fix(layout): adjust spacing

--- src/layout/container/Container.vue
+++ src/layout/container/Container.vue
@@ -3,10 +3,10 @@
     class="
       w-full
       min-h-[70dvh]
-      px-[1.8rem] py-12
-      xl:px-[20dvw] xl:py-16
-      lg:px-[10dvw] lg:py-12
-      md:px-[1.8rem] md:py-12
+      px-7 py-12
+      xl:px-[23dvw] xl:py-16
+      lg:px-[13dvw] lg:py-12
+      md:px-7 md:py-12
     "
   >
     <slot />
